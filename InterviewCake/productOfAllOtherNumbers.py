7a743ec3df26ed4b51b0b8050c30c1d0b1749cbc
anmolb <bajracha71@gmail.com>
2019-11-04 16:25:22 -0600

    modified code for clarity

--- InterviewCake/productOfAllOtherNumbers.py
+++ InterviewCake/productOfAllOtherNumbers.py
@@ -1,5 +1,5 @@
 # Product of All Other numbers:
-#-----------------------------#
+# -----------------------------#
 
 # You have a list of integers, and for each index you want to find the product of every integer except the integer at that index.
 
@@ -19,86 +19,79 @@
 import unittest
 
 
-def prod_before(int_list):
-
-    result = []
-    prod = 1
-    for x in int_list:
-        result.append(prod)
-        prod = prod * x 
-    
-    return result
-    
-
-
-def getProdutExceptSelf(int_list):
-    
-    array_size = len(int_list)
-    
-    if (array_size) <= 1:
-        raise Exception("integer list must contain atleast 2 elements")
-    
+def get_products_of_all_ints_except_at_index(int_list):
+    if len(int_list) <= 1:
+        raise Exception("error")
     # Make a list with the products
-    
-    prodBefore = prod_before(int_list)
-    
-    prod = 1
-    
-    end = array_size - 1
+    lprod = leftProduct(int_list)
+    rprod = rightProduct(int_list)
 
-    while end >= 0:
-        prodBefore[end] *=  prod
-        prod  *= int_list[end]
-        end -= 1
-    
-    return prodBefore
+    return list(map(lambda x, y: x * y, lprod, rprod))
+
+
+def leftProduct(nums):
+    n = len(nums)
+    res = [1] * n
+
+    for i in range(0, n - 1):
+        res[i + 1] = res[i] * nums[i]
+
+    return res
+
+
+def rightProduct(nums):
+    n = len(nums)
+    res = [1] * n
+
+    for i in range(n - 1, 0, -1):
+        res[i - 1] = res[i] * nums[i]
+
+    return res
 
 
 # Tests
-
 class Test(unittest.TestCase):
 
     def test_small_list(self):
-        actual = getProdutExceptSelf([1, 2, 3])
+        actual = get_products_of_all_ints_except_at_index([1, 2, 3])
         expected = [6, 3, 2]
         self.assertEqual(actual, expected)
 
     def test_longer_list(self):
-        actual = getProdutExceptSelf([8, 2, 4, 3, 1, 5])
+        actual = get_products_of_all_ints_except_at_index([8, 2, 4, 3, 1, 5])
         expected = [120, 480, 240, 320, 960, 192]
         self.assertEqual(actual, expected)
 
     def test_list_has_one_zero(self):
-        actual = getProdutExceptSelf([6, 2, 0, 3])
+        actual = get_products_of_all_ints_except_at_index([6, 2, 0, 3])
         expected = [0, 0, 36, 0]
         self.assertEqual(actual, expected)
 
     def test_list_has_two_zeros(self):
-        actual = getProdutExceptSelf([4, 0, 9, 1, 0])
+        actual = get_products_of_all_ints_except_at_index([4, 0, 9, 1, 0])
         expected = [0, 0, 0, 0, 0]
         self.assertEqual(actual, expected)
 
     def test_one_negative_number(self):
-        actual = getProdutExceptSelf([-3, 8, 4])
+        actual = get_products_of_all_ints_except_at_index([-3, 8, 4])
         expected = [32, -12, -24]
         self.assertEqual(actual, expected)
 
     def test_all_negative_numbers(self):
-        actual = getProdutExceptSelf([-7, -1, -4, -2])
+        actual = get_products_of_all_ints_except_at_index([-7, -1, -4, -2])
         expected = [-8, -56, -14, -28]
         self.assertEqual(actual, expected)
 
     def test_error_with_empty_list(self):
         with self.assertRaises(Exception):
-            getProdutExceptSelf([])
+            get_products_of_all_ints_except_at_index([])
 
     def test_error_with_one_number(self):
         with self.assertRaises(Exception):
-            getProdutExceptSelf([1])
+            get_products_of_all_ints_except_at_index([1])
 
 
 unittest.main(verbosity=2)
-
 
 # Complexity:
 # Time: O(n) where n is the length of array
