1d2aee25f72091b9c8e5a090f0600c07dd758243
anmolb <bajracha71@gmail.com>
2019-11-08 15:36:18 -0600

    add playgroud.py file

---
+++ playground.py
@@ -0,0 +1,7 @@
+# This file is used for prototyping
+
+s1 = 'Chocolate cake for dinner and pound cake for dessert'
+print(s1.split(" "))
+
+s2 = 'Mmm...mmm...decisions...decisions'
+print(s2.split([".", " "]))
