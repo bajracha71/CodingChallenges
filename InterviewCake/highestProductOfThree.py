9f8ecae83b64c42e56221c62296f5da79336e72c
anmolb <bajracha71@gmail.com>
2020-02-19 13:24:32 -0600

    implemented using heap

--- InterviewCake/highestProductOfThree.py
+++ InterviewCake/highestProductOfThree.py
@@ -5,9 +5,30 @@
 
 # The input list_of_ints will always have at least three integers.
 
+"""
+Idea:
+1. From list find top 3 numbers, h1, h2, h3 such that h1 >= h2 >= h3
+2. From list find lowest 2 number, l1 and l2 such that l1 <= l2:
+    ( To take core of negative numbers )
+3. result = max( h1 * h2 * h3, h1 * l1 * l2)
+
+"""
 import unittest
+import heapq
 
 def highest_product_of_3(list_of_ints):
+    if len(list_of_ints) < 3:
+        raise Exception("error")
+
+    # Calculate the highest product of three numbers
+
+    heapq.heapify(list_of_ints)
+    h1, h2, h3 = heapq.nlargest(3, list_of_ints)
+    l1, l2 = heapq.nsmallest(2, list_of_ints)
+
+    return max(h1 * h2 * h3, h1 * l1 * l2)
+
+def highest_product_of_3_old(list_of_ints):
 
     # Calculate the highest product of three numbers
     n = len(list_of_ints)
@@ -57,41 +78,41 @@ def get_lowest_2(nums):
 class Test(unittest.TestCase):
 
     def test_short_list(self):
-        acutal = highestProductOfThree([1,2,3,4])
+        actual = highest_product_of_3([1, 2, 3, 4])
         expected = 24
-        self.assertEqual(acutal, expected)
+        self.assertEqual(actual, expected)
 
     def test_longer_list(self):
-        actual = highestProductOfThree([6, 1, 3, 5, 7, 8, 2])
+        actual = highest_product_of_3([6, 1, 3, 5, 7, 8, 2])
         expected = 336
         self.assertEqual(actual, expected)
 
     def test_list_has_one_negative(self):
-        actual = highestProductOfThree([-5, 4, 8, 2, 3])
+        actual = highest_product_of_3([-5, 4, 8, 2, 3])
         expected = 96
         self.assertEqual(actual, expected)
 
     def test_list_has_two_negatives(self):
-        actual = highestProductOfThree([-10, 1, 3, 2, -10])
+        actual = highest_product_of_3([-10, 1, 3, 2, -10])
         expected = 300
         self.assertEqual(actual, expected)
 
     def test_list_is_all_negatives(self):
-        actual = highestProductOfThree([-5, -1, -3, -2])
+        actual = highest_product_of_3([-5, -1, -3, -2])
         expected = -6
         self.assertEqual(actual, expected)
 
     def test_error_with_empty_list(self):
         with self.assertRaises(Exception):
-            highestProductOfThree([])
+            highest_product_of_3([])
 
     def test_error_with_one_number(self):
         with self.assertRaises(Exception):
-            highestProductOfThree([1])
+            highest_product_of_3([1])
 
     def test_error_with_two_numbers(self):
         with self.assertRaises(Exception):
-            highestProductOfThree([1, 1])
+            highest_product_of_3([1, 1])
 
 
 unittest.main(verbosity=2)
